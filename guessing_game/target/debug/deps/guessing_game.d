537f45ad719ee26f08221d6a68faa16e96c0401d
guocheng <gc1365798@gmail.com>
2020-12-26 23:15:41 +0800

    Chapter 1 and 2

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\GCVictor\Git\The Rust Programming Language\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\GCVictor\Git\The Rust Programming Language\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\GCVictor\Git\The\ Rust\ Programming\ Language\guessing_game\target\debug\guessing_game.exe: C:\GCVictor\Git\The\ Rust\ Programming\ Language\guessing_game\src\main.rs
