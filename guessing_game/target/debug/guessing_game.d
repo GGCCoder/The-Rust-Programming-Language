e535191efa30f95d04af71ccd96bb82080091186
guocheng <gc1365798@gmail.com>
2020-12-29 14:16:38 +0800

    Delete target folders

--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-C:\GCVictor\Git\The\ Rust\ Programming\ Language\guessing_game\target\debug\guessing_game.exe: C:\GCVictor\Git\The\ Rust\ Programming\ Language\guessing_game\src\main.rs
--- hello_cargo/target/debug/deps/hello_cargo-b22c5f21db270d0b.d
+++
@@ -1,5 +0,0 @@
-C:\GCVictor\Git\The Rust Programming Language\hello_cargo\target\debug\deps\hello_cargo-b22c5f21db270d0b.rmeta: src\main.rs
-
-C:\GCVictor\Git\The Rust Programming Language\hello_cargo\target\debug\deps\hello_cargo-b22c5f21db270d0b.d: src\main.rs
-
-src\main.rs:
